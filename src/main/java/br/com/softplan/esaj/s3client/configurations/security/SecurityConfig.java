fff1307c44a314cf587f3cece34e0b1b4a7600fe
rafael.melges <rafael.melges@softplan.com.br>
2021-04-15 14:35:03 -0300

    inicia projeto e security

---
+++ src/main/java/br/com/softplan/esaj/s3client/configurations/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package br.com.softplan.esaj.s3client.configurations.security;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${s3Client.auth.basicAuth.userName}")
+	private String basicUserName;
+
+	@Value("${s3Client.auth.basicAuth.password}")
+	private String basicPassword;
+
+	@Value("${s3Client.cors.allowedOrigins}")
+	private String[] setAllowedOrigins;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser(basicUserName).password("{noop}".concat(basicPassword)).roles("APPLICATION");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(setAllowedOrigins));
+		configuration.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE"));
+		configuration.addAllowedHeader("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
